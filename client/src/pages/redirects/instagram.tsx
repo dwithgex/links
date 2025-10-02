abce8ec0480f6a6c40186ae763313388b384f65f
WithGex Dev <dev@withgex.com>
2025-10-02 05:00:27 +0200

    Add social media redirect routes for GitHub Pages

---
+++ client/src/pages/redirects/instagram.tsx
@@ -0,0 +1,5 @@
+import Redirect from "@/components/Redirect";
+
+export default function InstagramRedirect() {
+  return <Redirect url="https://www.instagram.com/withgex" platform="Instagram" />;
+}
---
+++ client/src/pages/redirects/tiktok.tsx
@@ -0,0 +1,5 @@
+import Redirect from "@/components/Redirect";
+
+export default function TikTokRedirect() {
+  return <Redirect url="https://www.tiktok.com/@gextrap" platform="TikTok" />;
+}
